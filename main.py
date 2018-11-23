c33a23e245643748844102eb57fe0343434d3421
Octavio <octaviomdq93@gmail.com>
2018-11-22 21:54:42 -0300

    spectrogram to identify dialed frequencies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy
+from scipy import signal
 import scipy.io.wavfile
 import matplotlib.pyplot as plt
 
@@ -8,8 +9,18 @@ def plot_signal():
     sample_rate, sample_array  = scipy.io.wavfile.read('modemDialing.wav')
     x_array = np.arange(len(sample_array))
     x_array_normalized = np.true_divide(x_array,sample_rate)
-    plt.plot(x_array_normalized,sample_array)
-    plt.savefig("graph.svg",dpi=1000, format='svg')
+    #plt.plot(x_array_normalized,sample_array)
+    #plt.savefig("graph.svg",dpi=1000, format='svg')
+    #plt.show()
+
+    return sample_rate, sample_array, x_array_normalized
+
+
+def spectrogram(x_axis,fs=1.0,):
+    f,t,Sxx = scipy.signal.spectrogram(x_axis,fs)
+    plt.pcolormesh(t,f,Sxx)
+    plt.ylabel('Frecuencia [Hz]')
+    plt.xlabel('Tiempo [s]')
     plt.show()
 
 def fundamental_dtmf_freq():
@@ -18,4 +29,5 @@ def fundamental_dtmf_freq():
 
 
 if __name__ == '__main__':
-    plot_signal()
+    fs,sample_array,normalized_x =plot_signal()
+    spectrogram(sample_array,fs)
